28e9e1b320c96bc8b9aa277238285e53ede3bef8
Courtney Campany <courtneycampany@gmail.com>
2018-06-15 12:39:24 -0400

    first commit, and started leaf chemistry formatting

---
+++ scripts/leafchem_format.R
@@ -0,0 +1,19 @@
+#format isotopes
+
+chem <- read.csv("raw_data/leaf_chem.csv")
+treat <- read.csv("raw_data/treatments.csv")
+
+
+age_format <- function(x) {
+    x$color <- gsub()
+}
+
+#remove id $
+chem$color <- gsub("\\d+-", "", chem$id)
+#remove leaf age color
+chem$plant_id <- gsub("-[A-Z]", "", chem$id)
+#add treatment
+chem <- merge(chem, treat)
+
+
+write.csv(chem, "calculated_data/leaf_chemistry.csv", row.names = FALSE)
