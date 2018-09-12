493110176b302beb7cc974c89c629e24c094065e
Court Campany <ccampany@colgate.edu>
2018-09-12 13:57:42 -0400

    added NOTES scripts so we can chat in the repo
    added isotope machine methods from cornell

---
+++ NOTES.R
@@ -0,0 +1,6 @@
+###NOTES:
+
+#Lets decide on a model formula to run for bivariate relationships and why:
+#Do we want a variable ~ varibale + year
+#             variable ~ variable * year
+#             variable(year1) ~ variable(year1)              
